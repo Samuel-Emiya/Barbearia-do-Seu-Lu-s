e1d4c678ad4420290032ea56b939f479875287b9
Samuel-Emiya <samuelmarinhocruz1@gmail.com>
2026-05-24 09:53:53 -0300

    Começando o js

--- pages/agendamento.js
+++ pages/agendamento.js
@@ -2,3 +2,8 @@ function validarFormulario(event) {
     document.getElementById("click").innerHTML = alert("Formulário Enviado")
 }
 
+// Começando a fazer uma página usando js
+
+function validarFormulario(event) {
+    document.getElementById("click").innerHTML = alert("Formulário Enviado")
+}
