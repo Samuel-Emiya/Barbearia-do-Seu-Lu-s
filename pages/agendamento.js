6a4c5353139cd62a8d34508c9a15fe1cd00700df
Samuel-Emiya <samuelmarinhocruz1@gmail.com>
2026-05-24 11:03:30 -0300

    Feito

--- pages/agendamento.js
+++ pages/agendamento.js
@@ -5,5 +5,19 @@ function validarFormulario(event) {
 // Começando a fazer uma página usando js
 
 function validarFormulario(event) {
-    document.getElementById("click").innerHTML = alert("Formulário Enviado")
+    event.preventDefault();
+
+    const formulario = document.forms["agendaMento"];
+
+    const dados = {
+        nome: formulario.nome.value,
+        telefone: formulario.telefone.value,
+        servico: formulario["serviço"].value,
+        data: formulario.data.value,
+        hora: formulario.hora.value
+    };
+
+    localStorage.setItem("agendamento", JSON.stringify(dados));
+
+    window.location.href = "receba.html";
 }
